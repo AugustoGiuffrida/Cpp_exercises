7969053ac5ad105fd61e200065bed6bf8d79167d
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2024-06-11 21:55:14 -0300

    add exercise_1

---
+++ Ej1/Prueba1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main (int argc, char const *argv[])
+{
+    std:: cout << "hello word" << std:: endl;
+    return 0;
+}
+
+//g++ Prueba1.cpp -o prueba
+
+//./prueba.exe
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# hola_mundo
+# C++ practice
