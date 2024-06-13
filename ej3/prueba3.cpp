7094a976d943dd4793b2da1096ef76490b0c2d33
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2024-06-13 17:58:38 -0300

    Exercises

---
+++ ej3/prueba3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+//Declarando constantes
+#define PI 3.1416
+
+int main(){
+
+    float altura = 1.81;
+    std::cout << altura << std::endl;
+    
+    const float GRAVEDAD = 9.81;
+    std::cout << GRAVEDAD  << std::endl;
+
+    std::cout << PI << std:: endl;
+
+    return 0;
+}
---
+++ ej4/prueba4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+
+    float 
+
+    return 0;
+}
